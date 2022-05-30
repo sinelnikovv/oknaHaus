643b65b4c294fcd0ad50197f30ebf5724745f828
sinelnikovv <aktep010@gmail.com>
2022-05-30 21:07:51 +0300

    tabs

---
+++ app.js
@@ -0,0 +1,10 @@
+var splide = new Splide('.splide', {
+  arrows: boolean = false,
+} );
+splide.mount();
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
