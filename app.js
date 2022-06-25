d18fbcd2f56bd21cc06c3ce807c8a6ec4506282f
sinelnikovv <aktep010@gmail.com>
2022-06-25 18:58:31 +0300

    finish

--- app.js
+++ app.js
@@ -74,7 +74,42 @@ var splide = new Splide( '#splidePortfolio', {
   pagination: boolean = false,
 }).mount();
 
+var splide = new Splide('#our_services_splide_1', {
+  type   : 'loop',
+  perPage: 1,
+  perMove: 1,
+  pagination: boolean = true,
+  arrows: false,
+}).mount();
+var splide = new Splide('#our_services_splide_2', {
+  type   : 'loop',
+  perPage: 1,
+  perMove: 1,
+  pagination: boolean = true,
+  arrows: false,
+}).mount();
+var splide = new Splide('#our_services_splide_3', {
+  type   : 'loop',
+  perPage: 1,
+  perMove: 1,
+  pagination: boolean = true,
+  arrows: false,
+}).mount();
+var splide = new Splide('#our_services_splide_4', {
+  type   : 'loop',
+  perPage: 1,
+  perMove: 1,
+  pagination: boolean = true,
+  arrows: false,
+}).mount();
 
+var splide = new Splide('#portfolio_splide_1', {
+  type   : 'loop',
+  perPage: 1,
+  perMove: 1,
+  pagination: boolean = true,
+  arrows: false,
+} ).mount();
 
 
 
